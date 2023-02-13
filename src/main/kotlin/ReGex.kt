b4c2cb57d971113756c184f5ef9ff21a5281d7ca
Orlando Nascimento <73672733+on-ferreira@users.noreply.github.com>
2023-02-13 19:22:03 -0300

    Update ReGex.kt
    Adicionado input do usuário para validação do padrão.

--- src/main/kotlin/ReGex.kt
+++ src/main/kotlin/ReGex.kt
@@ -10,7 +10,7 @@ fun main(){
     val patternCpf = Regex("[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2}")
     val patternCnpj = Regex("\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}")
     val patternCpfOrCnpj = Regex("[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2} | \\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}")
-    val patternMoedas = Regex("[RU][$]\\s[0-9]+[.,][0-9]{2}")
+    val patternMoedas = Regex("[RU][$][\\s0-9]+[.,][0-9]{2}")
     val patternEmail = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.(com|com.br)$")
 
     val patternList = listOf(patternCpf,patternCnpj,patternCpfOrCnpj,patternMoedas,patternEmail)
@@ -23,15 +23,22 @@ fun main(){
         patternEmail to "E-mail"
     )
 
-    val emailTest = "abc123@provedor.com"
-    val cpfTest = "123.456.789-00"
-    val cnpjTest = "12.345.678/0001-00"
-    val noPatternTest = "arroz"
-    val moedaTest = "U$ 123.58"
-    val listaTests = listOf(emailTest,cpfTest,cnpjTest,noPatternTest,moedaTest)
+//    val emailTest = "abc123@provedor.com"
+//    val cpfTest = "123.456.789-00"
+//    val cnpjTest = "12.345.678/0001-00"
+//    val noPatternTest = "arroz"
+//    val moedaTest = "U$ 123.58"
+//    val listaTests = listOf(emailTest,cpfTest,cnpjTest,noPatternTest,moedaTest)
+//
+//    listaTests.forEach{
+//        println("\"$it\" pertence ao padrão ${it.type(patternList, patternNames)}")
+//    }
 
-    listaTests.forEach{
-        println("\"$it\" pertence ao padrão ${it.type(patternList, patternNames)}")
+    var input:String = ""
+    while(!input.equals("Sair")){
+        println("Digite alguma coisa para ver se corresponde a algum padrão: ")
+        input = readln()
+        println(input.type(patternList, patternNames))
     }
 
 }
